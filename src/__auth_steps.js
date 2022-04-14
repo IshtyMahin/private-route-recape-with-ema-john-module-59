aae258a2feab409f36d1aa9806ccc8c3fe648f25
Ishtiaq uddin <ishty119@gmail.com>
2022-04-14 22:45:58 +0600

    install react firebase hook and implement create user

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -1,8 +1,13 @@
 /*
-  * 1. create a new project in console.firebase.google.com
-  * 2. npm install
+  * 1. create a new firebase project in console.firebase.google.com
+  * 2. npm install firebase
   * 3. create firebase.init.js and import getAuth to export auth
   * 4. firebase settings >  Authentication > enable Email and password auth
+  * 5. create Login ,signUp component, setup route
+  * 6. attach form field handler and form submit handler
+  * 7.npm install --save react-firebase-hooks
+  * 8. useCreateUserWithEmailAndPassword from react-firebase-hooks
+  * 
  */ 
 
 /* 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -9,16 +9,16 @@ const Login = () => {
         <form action="">
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="" />
+            <input type="email" name="email" id="" required />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="" />
+            <input type="password" name="password" id="" required/>
           </div>
           <input className="form-submit" type="submit" value="Submit" />
         </form>
         <p>
-            New to Ema-John? <Link className="form-link" to ="/signup">Create an account</Link>
+            New to Ema-John ? <Link className="form-link" to ="/signup">Create an account</Link>
         </p>
       </div>
     </div>
